b8e7cb175aea3ecac832b65774ac1a16e39a6dd9
Dave Page <dpage@pgadmin.org>
2016-06-10 15:23:26 +0100

    Further improvements to pip docs.

--- download/pip4.php
+++ download/pip4.php
@@ -19,9 +19,9 @@ $PAGE_CONTENT = '
 
 <p>' . _("To install, download the wheel and install with a command such as <code>pip install ./pgadmin4-1.0_beta1-py2-none-any.whl</code>. It is strongly recommended that a Python Virtual Environment is used.") . '</p>
 
-<p>' . _("Once installed, you will need to create a <i>config_local.py</i> file in the same directory as <i>config.py</i>. On a machine with a virtual environment created at <i>~/pgadmin4</i>, this is <i>lib/python2.7/site-packages/pgadmin4</i>. Ensure you set values for the SECRET_KEY, SECURITY_PASSWORD_SALT and CSRF_SESSION_KEY settings at bare minimum - see config.py for more information and other settings that can be customised. In order to reference other variables from <i>config.py</i>, you may need to include <code>from config import *</code> at the top of <config_local.py</p>") . '</p>
+<p>' . _("Once installed, you will need to create a <i>config_local.py</i> file in the same directory as <i>config.py</i>. On a machine with a virtual environment created at <i>~/pgadmin4</i>, this is <i>~/pgadmin4/lib/python2.7/site-packages/pgadmin4</i>. Ensure you set values for the SECRET_KEY, SECURITY_PASSWORD_SALT and CSRF_SESSION_KEY settings at bare minimum - see <i>config.py</i> for more information and other settings that can be customised. In order to reference other variables from <i>config.py</i>, you may need to include <code>from config import *</code> at the top of <i>config_local.py</i>.</p>") . '</p>
 
-<p>' . _("pgAdmin can now be run with a command like <code>python /path/to/virtualenv/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py</code>. Finally, point your browser to <i>http://127.0.0.1:5050</i>.") . '</p>
+<p>' . _("pgAdmin can now be run with a command like <code>python ~/pgadmin4/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py</code>. Finally, point your browser to <i>http://127.0.0.1:5050</i>.") . '</p>
 
 <p>' . _("Please see the <a href=\"/docs4/dev/server_deployment.html\">online documentation</a> for information on deploying pgAdmin 4 behind a web server.") . '</p>
 
