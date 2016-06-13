0bf7d88ef2fe8aa3696c17f827357c3d3c00ce30
Dave Page <dpage@pgadmin.org>
2016-06-13 10:56:21 +0100

    Link to the tracker from the support index page.

--- support/index.php
+++ support/index.php
@@ -11,6 +11,8 @@ $PAGE_CONTENT = '
 
 <p>' . sprintf(_("If you have a problem that you need help with, please make sure you check the <a href=\"%s\">Frequently Asked Questions</a> list as well as the pgAdmin documentation for a solution first. If you cannot find the answer you need there, then please try searching Google (a Google search facility is provided at the top of the page) next. Make sure you include any relevant error messages in your query. If you still cannot find the answer you need, then please post a message to the <a href=\"%s\">pgadmin-support</a> mailing list."), "faq.php", "list.php") . '</p>
 
+<p>' . _("<b>Note: </b>For pgAdmin 4 only, bugs should be logged on the <a href=\"https://redmine.postgresql.org/projects/pgadmin4/issues/new\">tracker</a>. General questions and discussion should remain on the mailing list. Questions logged on the tracker will be closed.") . '</p>
+
 <p>' . sprintf(_("Please <b>do not</b> send an email directly to one of the programmers; they might miss it, or even be the wrong person at all. Using the mailing list guarantees that your request isn't lost and will be answered in a timely fashion. Dave Page's <a href=\"%s\">blog</a> includes a frank posting describing why this is a bad idea, whilst Eric Raymond's paper <a href=\"%s\">'How to ask questions the smart way'</a> describes the best way to get the help you need."), "http://pgsnake.blogspot.co.uk/2005/04/support-rant-dave-goes-off-on-one.html", "http://www.catb.org/~esr/faqs/smart-questions.html") . '</p>
 '; 
 
