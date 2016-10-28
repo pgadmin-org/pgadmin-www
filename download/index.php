860c4b91e51f54f5c86c4599141bd0ffa7da69c5
Dave Page <dpage@pgadmin.org>
2016-10-28 15:51:37 +0100

    Further cleanup

--- download/index.php
+++ download/index.php
@@ -9,7 +9,7 @@ $PAGE_CONTENT = '
 
 <p>' . _("pgAdmin is a free software project released under the <a href=\"../licence.php\">PostgreSQL/Artistic licence</a>. The software is available in source and binary format from the <a href=\"https://www.postgresql.org/ftp/pgadmin3/\">PostgreSQL mirror network</a>. Because compiling from source requires technical knowledge, we recommend installing binary packages whenever possible.") . '</p>
 
-<p>' . _("The pages in this section give additional details about each binary package available as well as more direct download links. In addition, you can download source tarballs, additional language translations, pgAgent and admin packs for your servers to enable additional functionality.") . '</p>
+<p>' . _("The pages in this section give additional details about each binary package available as well as more direct download links. In addition, you can download source tarballs and pgAgent for your servers to enable additional functionality.") . '</p>
 
 <p>' . _("Finally, pgAdmin is included by default in many Linux distributions, such as RedHat, Fedora, Debian and Ubuntu. Please check your package manager for details.") . '</p>
 '; 
