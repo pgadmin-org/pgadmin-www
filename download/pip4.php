524d9f011c9e31f48de0a7b906bf363c9142ba9a
Dave Page <dpage@pgadmin.org>
2017-03-10 12:32:41 +0000

    Release 1.3.

--- download/pip4.php
+++ download/pip4.php
@@ -12,12 +12,13 @@ $PAGE_CONTENT = '
 <p>' . _("A Python wheel is available for those wishing to run pgAdmin as a web application in a Python environment. Note that the packages do not include the Desktop Runtime.") . '</p>
 
 <ul>
+  <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.3/pip/", "1.3") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.2/pip/", "1.2") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.1/pip/", "1.1") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.0/pip/", "1.0") . '<br />&nbsp;</li>
 </ul>
 
-<p>' . _("To install, download the wheel and install with a command such as <code>pip install ./pgadmin4-1.1-py2-none-any.whl</code>. It is strongly recommended that a Python Virtual Environment is used.") . '</p>
+<p>' . _("To install, download the wheel and install with a command such as <code>pip install ./pgadmin4-1.3-py2.py3-none-any.whl</code>. It is strongly recommended that a Python Virtual Environment is used.") . '</p>
 
 <p>' . _("pgAdmin can now be run with a command like <code>python ~/pgadmin4/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py</code> (assuming you use a virtual environment at ~/pgadmin4). Finally, point your browser to <i>http://127.0.0.1:5050</i>.") . '</p>
 
--- download/source4.php
+++ download/source4.php
@@ -12,6 +12,7 @@ $PAGE_CONTENT = '
 <p>' . _("Source tarballs are available for users wishing to compile their own copy of pgAdmin 4, or those using platforms for which we do not supply prebuilt packages.") . '</p>
 
 <ul>
+  <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.3/source/", "1.3") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.2/source/", "1.2") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.1/source/", "1.1") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">pgAdmin 4 v%s</a>"), "https://www.postgresql.org/ftp/pgadmin3/pgadmin4/v1.0/source/", "1.0") . '<br />&nbsp;</li>
@@ -20,6 +21,7 @@ $PAGE_CONTENT = '
 <p>' . _("Build instructions are included in the tarballs, and are available online:") . '</p>
 
 <ul>
+  <li>' . sprintf(_("<a href=\"%s\">v%s instructions</a>"), "https://git.postgresql.org/gitweb/?p=pgadmin4.git;a=blob_plain;f=README;hb=1e5de7e66e7e578a7803087389d18102e0f5d945", "1.3") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">v%s instructions</a>"), "https://git.postgresql.org/gitweb/?p=pgadmin4.git;a=blob_plain;f=README;hb=2dc0eab057d797a1fecb9947239be5638b442c5d", "1.2") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">v%s instructions</a>"), "https://git.postgresql.org/gitweb/?p=pgadmin4.git;a=blob_plain;f=README;hb=6c45eea1a04dc911a9a5bc3f2d6de04caadba6b6", "1.1") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">v%s instructions</a>"), "https://git.postgresql.org/gitweb/?p=pgadmin4.git;a=blob_plain;f=README;hb=30397476da45e419a409710e7447bf0f10fcbcd9", "1.0") . '<br />&nbsp;</li>
