10f2959b2442f1896b458d916e9c58b268d673ce
Dave Page <dpage@pgadmin.org>
2017-04-18 12:37:47 +0100

    Correct link to downloads, per Devrim.

--- download/index.php
+++ download/index.php
@@ -7,7 +7,7 @@ $PAGE_CONTENT = '
 
 <h1>' . _("Introduction") . '</h1>
 
-<p>' . _("pgAdmin is a free software project released under the <a href=\"../licence.php\">PostgreSQL/Artistic licence</a>. The software is available in source and binary format from the <a href=\"https://www.postgresql.org/ftp/pgadmin3/\">PostgreSQL mirror network</a>. Because compiling from source requires technical knowledge, we recommend installing binary packages whenever possible.") . '</p>
+<p>' . _("pgAdmin is a free software project released under the <a href=\"../licence.php\">PostgreSQL/Artistic licence</a>. The software is available in source and binary format from the <a href=\"https://www.postgresql.org/ftp/pgadmin/\">PostgreSQL mirror network</a>. Because compiling from source requires technical knowledge, we recommend installing binary packages whenever possible.") . '</p>
 
 <p>' . _("The pages in this section give additional details about each binary package available as well as more direct download links. In addition, you can download source tarballs and pgAgent for your servers to enable additional functionality.") . '</p>
 
